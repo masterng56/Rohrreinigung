0fcd81692ce888df1404a94143ee3dc629be511f
masterng <masterng@yandex.ru>
2021-12-11 18:36:31 +0500

    Закончил разметку

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.slider').slick({
-        arrows: true,
-        dots: true,
+        arrows: false,
+        dots: false,
         autoplay: true,
         speed: 2000,
         autoplaySpeed: 3000,
@@ -12,14 +12,5 @@ $(document).ready(function() {
                 variableWidth: true,
             }
         }]
-    });
-
-    $('.top_menu_burger').click(function(event) {
-        $('.top_header_nav, .top_logo, .top_menu_burger, .top_nav-menu').toggleClass('active');
-    });
-
-    $('.bottom_menu_burger').click(function(event) {
-        $('.bottom_header_nav, .bottom_logo, .bottom_menu_burger, .bottom_nav-menu, .footer').toggleClass('active');
-        $('.bottom_menu_burger_text, .copywhrite, bottom_menu_burger_text').toggleClass('display_none');
     });
 });
